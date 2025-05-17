356ad4d5c553cefa6d43804e7a40e4f254054256
Aaroh2003 <aarohharkut308@gmail.com>
2025-05-17 20:34:44 +0530

    Mobile

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-// App.jsx - Updated with feature detail routes
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
-import FeaturesPage from './FeaturesPage';
-import FeatureDetailPage from './FeatureDetailPage';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import HomePage from "./HomePage";
+import FeaturesPage from "./FeaturesPage";
+import FeatureDetailPage from "./FeatureDetailPage";
+// import "./App.css";
 
+const App = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
-function App() {
+  useEffect(() => {
+    // Check if the user is on a mobile device
+    const checkMobile = () => {
+      return /iphone|ipod|android|blackberry|opera mini|windows phone|mobile/i.test(navigator.userAgent.toLowerCase());
+    };
+    setIsMobile(checkMobile());
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/features" element={<FeaturesPage />} />
-        <Route path="/features/:featureId" element={<FeatureDetailPage />} />
+        {/* Redirect to mobile site if needed */}
+        {isMobile ? (
+          window.location.replace("https://m.advoai.in")
+        ) : (
+          <>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/features" element={<FeaturesPage />} />
+            <Route path="/features/:featureId" element={<FeatureDetailPage />} />
+          </>
+        )}
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
